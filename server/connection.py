8d73aee895c682d1fac73d40a5e35f7560e07c59
NiumXp <formigacomcaimbra@gmail.com>
2021-08-25 22:47:53 -0300

    draft Connection and Node classes

---
+++ server/connection.py
@@ -0,0 +1,65 @@
+import asyncio
+import typing as t
+
+
+class Node:
+    __slots__ = ("value", "_next", "_prev",)
+
+    def __init__(
+        self,
+        value: t.Any, *,
+        _next: t.Optional["Node"] = None,
+        _prev: t.Optional["Node"] = None,
+    ) -> None:
+        self.value = value
+
+        self._next = _next
+        self._prev = _prev
+
+    @property
+    def tail(self) -> "Node":
+        node = self
+
+        while node._next:
+            node = node._next
+
+        return node
+
+    def bound(self, value: t.Any) -> "Node":
+        cls = self.__class__
+
+        node = self.tail
+        prev = self if node is self else node
+
+        node._next = cls(value, _prev=prev)
+
+        return node._next
+
+    def unbound(self) -> None:
+        if not self._prev:  # self is head
+            if self._next:
+                self._next._prev = None
+
+            return
+
+        if not self._next:  # self is tail
+            return setattr(self._prev, "_next", None)
+
+        self._prev._next = self._next
+        self._next._prev = self._prev
+
+
+class Connection:
+    def __init__(self) -> None:
+        self._listeners = []
+
+    def dispatch(self, event: str, data: t.Dict[str, t.Any]):
+        pass
+
+    def wait_for(self, event: str, *, timeout: int = 5):
+        future = asyncio.Future()
+        listener = (event, future)
+
+        self._listeners.append(listener)
+
+        return asyncio.wait_for(future, timeout=timeout)
